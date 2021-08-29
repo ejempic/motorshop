b3cd36db08a7b84501a332de1011bb8c2d90a2f0
Earl <empicearl@gmail.com>
2021-08-30 06:20:38 +0800

    fix errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -56,11 +56,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-
-        if(config('app.env') == 'local'){
-            return parent::render($request, $exception);
-        }else{
-            abort(404);
-        }
+        return parent::render($request, $exception);
     }
 }
