21e3e461c0eaf6fdd9ab21d31cf3268fa5ff4ecc
Earl <empicearl@gmail.com>
2022-01-14 19:09:15 +0800

    api for units

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', function (Request $request) {
     return "TEST";
 });
+Route::get('/units', function (Request $request) {
+
+    $unitQuery = \App\Units::query();
+    if($request->has('brand') && $query = $request->input('brand')){
+        $unitQuery->where('brand', 'like', '%'.$query.'%');
+    }
+    if($request->has('model') && $query = $request->input('model')){
+        $unitQuery->where('model', 'like', '%'.$query.'%');
+    }
+    if($request->has('color') && $query = $request->input('color')){
+        $unitQuery->where('color', 'like', '%'.$query.'%');
+    }
+    if($request->has('chassis_no') && $query = $request->input('chassis_no')){
+        $unitQuery->where('chassis_no', 'like', '%'.$query.'%');
+    }
+    if($request->has('bnew_repo') && $query = $request->input('bnew_repo')){
+        $unitQuery->where('bnew_repo', 'like', '%'.$query.'%');
+    }
+    $units = $unitQuery->get();
+
+    return response()->json([
+        'data'=>$units,
+        'status' => 'success'
+    ]);
+});
